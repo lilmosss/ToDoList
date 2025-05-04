20bb49332f62004012dc8f6bae878ea2999723ce
Lily Mosisa <lilymosisa13@gmail.com>
2025-05-04 12:09:43 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Lily Mosisa on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
